071f945461b419fd307b16c08fb1033d8faac78b
hoox276 <mebox.dev@gmail.com>
2026-04-01 18:14:01 -0300

    destacar texto 'em Minutos' em amarelo

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -9,9 +9,24 @@ const labelVariants = cva("text-sm font-medium leading-none peer-disabled:cursor
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
-));
+>(({ className, children, ...props }, ref) => {
+  // Envolvendo texto específico 'em Minutos' para aplicar classe amarela
+  const modifiedChildren = typeof children === 'string' ?
+    children.replace(/em Minutos/g, '<span class="text-yellow-500">em Minutos</span>') :
+    children;
+
+  return (
+    <LabelPrimitive.Root
+      ref={ref}
+      className={cn(labelVariants(), className)}
+      {...props}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={typeof modifiedChildren === 'string' ? { __html: modifiedChildren } : undefined}
+    >
+      {typeof modifiedChildren !== 'string' && modifiedChildren}
+    </LabelPrimitive.Root>
+  );
+});
 Label.displayName = LabelPrimitive.Root.displayName;
 
 export { Label };
